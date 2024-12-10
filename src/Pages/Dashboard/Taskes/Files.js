ed85b2f8716c81f41914064324f9a80e4f1cce6c
azizaldhubri <azizaldhubri@gmail.com>
2024-12-11 01:23:42 +0300

    Update Files.js

--- src/Pages/Dashboard/Taskes/Files.js
+++ src/Pages/Dashboard/Taskes/Files.js
@@ -6,7 +6,7 @@ export const typeFile=[
         type:'img',
         src_type:'image/jpeg',             
          pathimg:'https://free-001.yemenhosting.com/backend3/storage/app/public/assets',                   
-        pathDownload:'https://free-001.yemenhosting.com/backend3/storage/app/public/assets/download' ,
+        pathDownload:'https://free-001.yemenhosting.com/backend3/storage/app/public/assets' ,
             // pathDownload:'https://free-001.yemenhosting.com/api/download',
         
             // pathDownload:'https://free-001.yemenhosting.com/backend3/storage/app/public/assets',
